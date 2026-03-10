0bd7b89c7aa0bc629607601b0e7e3bbbc34e7cd9
merki86 <nassen.d230@gmail.com>
2026-03-10 11:09:07 +0500

    proper time computation and display

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ height = 600
 
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
+
+time = 0
+time_sec = 0
+delta = 0
+
 running = True
 
 gravity = 9.8
@@ -25,7 +30,18 @@ class Ball:
     def draw(self) -> None:
         pygame.draw.circle(screen, self.color, (self.position_x, self.position_y), self.radius)
 
+class Stopwatch:
+    def __init__(self) -> None:
+        self.multiplier = 1
+
+        self.font = pygame.font.SysFont("Arial", size=32)
+
+    def draw(self, delta: int) -> None:
+        output = pygame.font.Font.render(self.font, f"Time elapsed (s): {time_sec} Delta: {delta}", False, (255, 255, 255))
+        screen.blit(output, (0,height - output.get_rect().height))
+
 ball = Ball()
+stopwatch = Stopwatch()
 
 while running:
     for event in pygame.event.get():
@@ -36,20 +52,21 @@ while running:
 
     # Render
     ball.draw()
+    stopwatch.draw(delta)
 
     pygame.display.flip()
 
     # Logic
-    time_passed = pygame.time.get_ticks() / 30 / 5
 
-    new_position_y = int(1/2*ball.acceleration*(time_passed**2)) # h = 1/2*gt2
+    new_position_y = int( 0.5*ball.acceleration*(time_sec**2) * 100 ) # h = 1/2*gt2 100 HERE IS 100 px = 1 m
 
     if new_position_y >= height:
         new_position_y = height
 
     ball.position_y = new_position_y
 
-    clock.tick(30)
-    print(time_passed)
+    delta = clock.tick(60)
+    time += delta
+    time_sec = time / 1000
 
 pygame.quit()
