aee1dd941134d058a2cbf58c9bf72b17e21e9eef
merki86 <nassen.d230@gmail.com>
2026-05-27 19:23:30 +0500

    center of the object

--- object.py
+++ object.py
@@ -13,6 +13,7 @@ class Object:
         self.surface.fill((0, 0, 0, 0))
 
         pygame.draw.circle(self.surface, (255, 255, 0), (self.position.x*pixels_in_meter, self.position.y*pixels_in_meter), self.radius*pixels_in_meter)
+        pygame.draw.circle(self.surface, (255, 0, 255), (self.position.x*pixels_in_meter, self.position.y*pixels_in_meter), pixels_in_meter//5)
 
         dy = 0**2 + 0.5*9.8*time**2 # y = ut + (1/2)gt2 , in meters
         if self.position.y + dy + self.radius >= canvas_height:
